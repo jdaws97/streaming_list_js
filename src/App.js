4c8c7c7ef6192b4e6c3ca4ebf03bce2bbc316336
jdaws97 <jarod.daws@gmail.com>
2022-01-14 19:04:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createSession } from "./Backend/API";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
+import Nav from "./Components/Nav";
+import Search from "./Components/Search";
+import Results from "./Components/Results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Search />
+        <Routes>
+          <Route path="/search/:searchresult" element={Search} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
